a66f2b7611accbcbf5d8a8e51846d43dc5efba48
Raman Maksimchuk <Raman_Maksimchuk@epam.com>
2018-01-16 16:55:18 +0300

    Implement "Example with children"

--- ReactAndReduxMeetup/RRM-1-ReactBasics-SkeletonApp/my-app/src/HelloWorld.js
+++ ReactAndReduxMeetup/RRM-1-ReactBasics-SkeletonApp/my-app/src/HelloWorld.js
@@ -7,7 +7,26 @@ var WorldElement = React.createElement(
     "Hello World!"
 );
 
-// Go to index.js
+// #1 : Go to index.js
 //ReactDOM.render(WorldElement, document.getElementById("container"));
+//export default WorldElement;
 
-export default WorldElement;
+// ------------------------------------------------------------------
+var elem1 = React.createElement(
+    "h1",
+    {id:"id001", className:"my_title"},
+    "Hi World!"
+);
+
+var elem2 = React.createElement(
+    "div",
+    {id:"id002", className:"my_description..."},
+    "Some description..."
+);
+
+// #2 : Go to index.js
+var WorldElement2 = React.createElement("div", null, elem1, elem2);
+
+export { WorldElement, WorldElement2 };
+
+//ReactDOM.render(root, document.getElementById("container"));
--- ReactAndReduxMeetup/RRM-1-ReactBasics-SkeletonApp/my-app/src/index.js
+++ ReactAndReduxMeetup/RRM-1-ReactBasics-SkeletonApp/my-app/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import WorldElement from './HelloWorld';
+
+import {WorldElement, WorldElement2} from './HelloWorld';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// #1
 ReactDOM.render(WorldElement, document.getElementById("container"));
+
+// #2
+ReactDOM.render(WorldElement2, document.getElementById("container2"));
 
 registerServiceWorker();
