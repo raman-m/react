944a0739648a9bb8c3c8cd1ad7763c4e6a8a6e82
Raman Maksimčuk <dotnet044@gmail.com>
2019-04-19 18:20:48 +0300

    [ES6] 1. Importing styled components

--- lyn-es6-es2016/src/App.js
+++ lyn-es6-es2016/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import {Markup, Editor, Container, Column, Row, RuleInput, RuleLabel, StyleInput, Button, Document} from './styled'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hi!</h1>
-      </div>
+      <Container>
+        <Column>
+          <Button>
+            New Rule
+          </Button>
+        </Column>
+        <Column>
+          <Button>
+            Random Text
+          </Button>
+          <Document>
+            <Editor/>
+            <Markup/>
+          </Document>
+        </Column>
+      </Container>
     );
   }
 }
