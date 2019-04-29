1033e608b5b9fbafe1e944a47d81f1815bb80639
Raman Maksimčuk <dotnet044@gmail.com>
2019-04-29 16:55:27 +0300

    [ES6] 4. Apply random color for a rule

--- lyn-es6-es2016/src/App.js
+++ lyn-es6-es2016/src/App.js
@@ -132,6 +132,10 @@ class App extends Component {
 
     let newStyles = "".concat(styles).replace(",", "")
 
+    while (newStyles.includes('random')) {
+      newStyles = newStyles.replace('random', rando.color())
+    }
+    
     return newStyles
   }
 
