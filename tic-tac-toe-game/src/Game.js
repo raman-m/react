c8790ebd1b4b3ef1a6628d1709f1b11ff552735a
Raman Maksimčuk <dotnet044@gmail.com>
2019-04-30 19:27:06 +0300

    [TTT] Showing the Past Moves. Feature: Adding Time Travel

--- tic-tac-toe-game/src/Game.js
+++ tic-tac-toe-game/src/Game.js
@@ -48,11 +48,24 @@ export default class Game extends React.Component {
         });
     }
 
+    jumpTo(move) {}
+
     render() {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+              'Go to move #' + move :
+              'Go to game start';
+            return (
+              <li>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+              </li>
+            );
+        });
+      
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -70,7 +83,7 @@ export default class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
