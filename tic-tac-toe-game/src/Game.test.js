76b4f7c5d117add2cf0e8943b6674eaa7387d56a
Raman Maksimčuk <dotnet044@gmail.com>
2019-06-06 19:15:56 +0300

    [TTT] #1 Add test for non-array args

--- tic-tac-toe-game/src/Game.test.js
+++ tic-tac-toe-game/src/Game.test.js
@@ -1,6 +1,17 @@
 import { makeStepsDiff, NoDiff } from './Game'
 
 describe('Game component', () => {
+    it('makeStepsDiff should throw TypeError exception having non-array args', () => {
+        expect(() => makeStepsDiff())
+            .toThrowError(TypeError());
+        expect(() => makeStepsDiff(1, '[]'))
+            .toThrowError(TypeError());
+        expect(() => makeStepsDiff({}, []))
+            .toThrowError(TypeError());
+        expect(() => makeStepsDiff([], {}))
+            .toThrowError(TypeError());
+    });
+
     it('makeStepsDiff returns no diff for empty arrays', () => {
         expect(makeStepsDiff([], []))
             .toEqual(NoDiff);
